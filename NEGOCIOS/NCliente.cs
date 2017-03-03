65581c619d678700c85e708af38b2d989e71457f
frankjgp <frank.jgp14@gmail.com>
2017-03-03 13:51:33 -0500

    mant cliente

---
+++ NEGOCIOS/NCliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTIDAD;
+using DATOS;
+
+namespace NEGOCIOS
+{
+    public static class NCliente
+    {
+        public static int ActualizarClientes(ECliente ent)
+        {
+            return DCliente.ActualizarClientes(ent);
+        }
+
+        public static List<ECliente> ListarClientes(ECliente ent)
+        {
+            return DCliente.ListarClientes(ent);
+        }
+    }
+}
