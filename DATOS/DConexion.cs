616ced1864e8f6acfef3ec7c23f7bf080217e3c1
frankjgp <frank.jgp14@gmail.com>
2017-03-02 23:57:32 -0500

    Login

---
+++ DATOS/DConexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DATOS
+{
+    public static class DConexion
+    {
+        public enum DataBase
+        {
+            CnLasVegas = 1
+        }
+
+        public static string Get_Connection(DataBase tipo)
+        {
+            switch (tipo)
+            {
+                case DataBase.CnLasVegas:
+                    return ConfigurationManager.ConnectionStrings["CnnLasVegas"].ConnectionString;
+                default:
+                    return String.Empty;
+            }
+        }
+    }
+}
