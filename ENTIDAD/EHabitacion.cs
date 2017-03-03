29a968a46508985ee53bf477535388e8ea0b47aa
franki159 <chara.20.90@gmail.com>
2017-03-02 23:36:28 -0500

    zxczc

---
+++ ENTIDAD/EHabitacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDAD
+{
+    public class EHabitacion
+    {
+        public int ID_HABITACION { get; set; }
+        public string NUMERO { get; set; }
+        public int ID_TIPO_HABITACION { get; set; }
+        public decimal PRECIO { get; set; }
+        public int ID_LOCAL { get; set; }
+        public string DESCRIPCION { get; set; }
+    }
+}
---
+++ ENTIDAD/EReserva.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDAD
+{
+    public class EReserva
+    {
+        public int ID_ATENCION { get; set; }
+        public int ID_RESERVA { get; set; }
+        public int ID_CLIENTE { get; set; }
+        public int ID_HABITACION { get; set; }
+        public DateTime FEC_INI { get; set; }
+        public DateTime FEC_FIN { get; set; }
+        public int ID_TIPO_RESERVA { get; set; }
+        public string TIPO_RESERVA { get; set; }
+        public decimal ADELANTO { get; set; }
+        public decimal PRECIO_HAB { get; set; }
+        public decimal TOTAL_HAB { get; set; }
+        public int ID_MEDIO_PAGO { get; set; }
+        public string MEDIO_PAGO { get; set; }
+        public string OBSERVACION { get; set; }
+        public int ID_LOCAL { get; set; }
+
+    }
+}
